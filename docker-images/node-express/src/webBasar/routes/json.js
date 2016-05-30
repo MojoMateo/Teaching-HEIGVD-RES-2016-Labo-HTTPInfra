bac6ca029b72d01b7fb1f4c95fed18caabc301ad
Mathieu Urstein <mathieu.urstein@gmail.com>
2016-05-30 23:41:28 +0200

    création d'un dockerfile avec nodejs, fichiers sources express, avec modèle route/vue, sortie en json d'information de personnes aléatoires

---
+++ docker-images/node-express/src/webBasar/routes/json.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var Chance = require('chance');
+// Instantiate Chance so it can be used
+var chance = new Chance();
+
+/* GET /json. */
+router.get('/', function(req, res, next) {
+    var jsonRes = {};
+    jsonRes.name = chance.name();
+    jsonRes.age = chance.age();
+    jsonRes.avatar = chance.avatar();
+    jsonRes.favImage = "http://lorempixel.com/400/200/"+chance.word();
+    res.json(jsonRes);
+});
+
+module.exports = router;
